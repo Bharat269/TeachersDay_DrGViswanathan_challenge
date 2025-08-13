5e971847f3990fe4c70d1c81c1b5bfe4522a1471
Bharat269 <bharatmishra269@gmail.com>
2025-08-13 18:39:56 +0530

    13th August

---
+++ cpp_codes/translation.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string s;
+    string t;
+    cin>>s>>t;
+    if(s.size()!=t.size()){
+        cout<<"NO"<<endl;
+        return 0;
+    }
+    int n = s.size();
+    for(int i =0;i<s.size();i++){
+        if(s[i]!=t[n-i-1]){
+            cout<<"NO"<<endl;
+            return 0;
+        }
+    }
+    cout<<"YES"<<endl;
+}
